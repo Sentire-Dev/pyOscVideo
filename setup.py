8647f283cd31fd73d9f4e1ef0bfde7868a799911
Bruno Gola <me@bgo.la>
2020-02-20 16:32:29 +0100

    use setup.py to define gui script

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='pyoscvideo', version='0.1', packages=['pyoscvideo'])
+setup(name='pyoscvideo',
+      version='0.1.0',
+      packages=find_packages(),
+      entry_points={
+          'gui_scripts': [
+              'pyoscvideo = pyoscvideo.__main__:main'
+          ]
+      },
+      )
