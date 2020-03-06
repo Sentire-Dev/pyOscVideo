76d4abbdaa74ccff2b498ab720b4364316b3b782
audiophil <audiophil@posteo.de>
2020-03-06 13:47:48 +0100

    Program is now again callable as script

--- pyoscvideo/__main__.py
+++ pyoscvideo/__main__.py
@@ -6,9 +6,11 @@ TODO:
     - add license
 """
 import sys
+
 from PyQt5.QtWidgets import QApplication
-from pyoscvideo.model.model import Recorder
+
 from pyoscvideo.controllers.main_ctrl import MainController
+from pyoscvideo.model.model import Recorder
 from pyoscvideo.views.main_view import MainView
 from pyoscvideo.views.osc_interface import OSCInterface
 
@@ -32,3 +34,7 @@ def main():
     """Start the application."""
     app = App(sys.argv)
     app.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
