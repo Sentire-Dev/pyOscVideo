00b2603ffd33de42d30d2084e4e7701c4716d8fd
Bruno Gola <me@bgo.la>
2020-06-12 19:29:05 +0200

    generic way to convert any .ui files

--- generate_ui.sh
+++ generate_ui.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-pyuic5 pyoscvideo/gui/resources/main_view.ui -o pyoscvideo/gui/main_view_ui.py
-pyuic5 pyoscvideo/gui/resources/camera_view.ui -o pyoscvideo/gui/camera_view_ui.py
+for ui_file in pyoscvideo/gui/resources/*ui; do
+    pyuic5 "${ui_file}" -o "$(echo ${ui_file} | sed 's/resources\///' | sed 's/.ui$/_ui.py/')"
+done
